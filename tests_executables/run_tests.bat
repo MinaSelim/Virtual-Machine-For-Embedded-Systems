43ea8fd0697a9a9ae11785780381436d67d2862c
steveiaco <31911579+steveiaco@users.noreply.github.com>
2020-11-25 22:39:25 -0500

    Tests passing, changed VS configurations

--- tests_executables/run_tests.bat
+++ tests_executables/run_tests.bat
@@ -1,17 +1,17 @@
 @echo Running Code Patterns Txx...
 @if exist Txx.txt del Txx.txt
-@..\x64\Release\cm T01.exe > Txx.txt
-@..\x64\Release\cm T02.exe >> Txx.txt
-@..\x64\Release\cm T03.exe >> Txx.txt
-@..\x64\Release\cm T04.exe >> Txx.txt
-@..\x64\Release\cm T05.exe >> Txx.txt
-@..\x64\Release\cm T06.exe >> Txx.txt
-@..\x64\Release\cm T07.exe >> Txx.txt
-@..\x64\Release\cm T08.exe >> Txx.txt
-@..\x64\Release\cm T09.exe >> Txx.txt
-@..\x64\Release\cm T10.exe >> Txx.txt
-@..\x64\Release\cm T11.exe >> Txx.txt
-@..\x64\Release\cm T12.exe >> Txx.txt
+@..\Release\cm T01.exe > Txx.txt
+@..\Release\cm T02.exe >> Txx.txt
+@..\Release\cm T03.exe >> Txx.txt
+@..\Release\cm T04.exe >> Txx.txt
+@..\Release\cm T05.exe >> Txx.txt
+@..\Release\cm T06.exe >> Txx.txt
+@..\Release\cm T07.exe >> Txx.txt
+@..\Release\cm T08.exe >> Txx.txt
+@..\Release\cm T09.exe >> Txx.txt
+@..\Release\cm T10.exe >> Txx.txt
+@..\Release\cm T11.exe >> Txx.txt
+@..\Release\cm T12.exe >> Txx.txt
 type Txx.txt
 @echo on
 @echo Checking test result...
