07b96063548779eaaf7febf54af5a59a2b96ff66
Mina Selim <supermina55555@hotmail.com>
2020-11-26 22:29:27 -0500

    Isolated BSL layer and HAL layer for console output.

---
+++ Cm/bsl_output.h
@@ -0,0 +1,14 @@
+#ifndef BSL_OUTPUT_WINDOWS_H
+#define BSL_OUTPUT_WINDOWS_H
+
+#include "_stdtype.h"
+
+void bsl_COut_PutB(bool b);
+void bsl_COut_PutC(char c);
+void bsl_COut_PutS(const char* s);
+void bsl_COut_PutI(i32  i);
+void bsl_COut_PutU(u32  u);
+void bsl_COut_PutX(u32  u);
+void bsl_COut_PutN(void);
+
+#endif // !BSL_OUTPUT_WINDOWS_H
---
+++ Cm/bsl_output_host.c
@@ -0,0 +1,42 @@
+#ifdef HOST
+
+#include "bsl_output.h"
+#include <stdio.h>
+
+void bsl_COut_PutB(bool b)
+{
+	printf("%s", b ? "true" : "false");
+}
+
+void bsl_COut_PutC(char c)
+{
+	printf("%c", c);
+}
+
+void bsl_COut_PutS(const char* s)
+{
+	printf("%s", s);
+}
+
+void bsl_COut_PutI(i32 i)
+{
+	printf("%ld", i);
+}
+
+void bsl_COut_PutU(u32 u)
+{
+	printf("%lu", u);
+}
+
+void bsl_COut_PutX(u32 u)
+{
+	printf("%08lX", u);
+}
+
+void bsl_COut_PutN(void)
+{
+	printf("\n");
+}
+
+
+#endif
