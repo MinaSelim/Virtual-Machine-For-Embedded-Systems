afcbd395cfd2e532e333fa4238f8668479fcf1c7
Mina Selim <supermina55555@hotmail.com>
2020-12-03 21:07:34 -0500

    task 6-7 barebones

--- Cm/bsl_interrupt_Atmega328p.c
+++ Cm/bsl_interrupt_Atmega328p.c
@@ -1,14 +1,16 @@
 #ifdef __AVR_ATmega328P__
 #include "bsl_interrupt.h"
+#include <avr/io.h>
+#include <avr/interrupt.h>
 
 void bsl_Interrupt_Disable(void)
 {
-
+	cli();
 }
 
 void bsl_Interrupt_Enable(void)
 {
-
+	sei();
 }
 
 u16 bsl_Interrupt_SaveAndDisable(void)
@@ -28,6 +30,6 @@ void bsl_Interrupt_SetVector(u8 number, u32 handlerAddr)
 
 u32 bsl_Interrupt_GetVector(u8 number)
 {
-
+	return 0;
 }
 #endif
--- Cm/bsl_io_reg_Atmega328p.c
+++ Cm/bsl_io_reg_Atmega328p.c
@@ -1,13 +1,44 @@
 #ifdef __AVR_ATmega328P__
 #include "bsl_io_reg.h"
+#include <avr/io.h>
+
+#define B 1
+#define C 2
+#define D 3
+
 
 u32 bsl_IOReg_Read(u32 ioreg)
 {
-	return 0;
+	switch (ioreg)
+	{
+		case B:
+			DDRB = 0;
+			return PORTB;
+		case C:
+			DDRC = 0;
+			return PORTC;
+		case D:
+			DDRD = 0;
+			return PORTD;
+		default:
+			return 0;
+	}
+
 }
 
 void bsl_IOReg_Write(u32 ioreg, u32 value)
 {
-
+	switch (ioreg)
+	{
+	case B:
+		DDRB = 0xff;
+		PORTB = value;
+	case C:
+		DDRB = 0xff;
+		PORTB = value;
+	case D:
+		DDRC = 0xff;
+		PORTC = value;
+	}
 }
 #endif
